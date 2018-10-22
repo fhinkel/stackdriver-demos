555716ee373f772f8c2a1d3611c053fc1903f3b4
Franziska Hinkelmann <franzih@chromium.org>
2018-10-22 18:55:19 -0300

    Add app.yaml and log message

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+  console.log({message: 'Hello from Express'});
   res.send('Hello World for Stackdriver Logging!');
 });
 
