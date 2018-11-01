a98e06593247d68679a7b50633667aebe89d4d85
Franziska Hinkelmann <franzih@chromium.org>
2018-11-01 17:06:51 -0400

    Add endpoints that throw errors

--- index.js
+++ index.js
@@ -2,11 +2,53 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;;
 
-app.get('/', (req, res) => {
-  console.log({message: 'Hello from Express'});
-  res.send('Hello World for Stackdriver Logging!');
+app.get('/error', (req, res) => {
+  console.log('Hit the endpoint that throw and error.');
+  res.send('This threw an error');
+  throw new Error('Throw an error');
 });
 
-app.listen(port, () => 
+app.get('/error1', (req, res) => {
+  console.log('Hit the endpoint that throw and error.');
+  res.send('This threw an error');
+  throw new Error('Throw an error #1');
+});
+
+app.get('/error2', (req, res) => {
+  console.log('Hit the endpoint that throw and error.');
+  res.send('This threw an error');
+  throw new Error('Throw an error #2');
+});
+
+app.get('/crash', (req, res) => {
+  console.log('Hit the endpoint that crashes the app.');
+  res.send('This crashed everything');
+  setTimeout(function () {
+    throw new Error('We crashed!');
+  }, 5);
+});
+
+app.get('/crash1', (req, res) => {
+  console.log('Hit the endpoint that crashes the app.');
+  res.send('This crashed everything');
+  setTimeout(function () {
+    throw new Error('We crashed #1!');
+  }, 5);
+});
+
+app.get('/crash2', (req, res) => {
+  console.log('Hit the endpoint that crashes the app.');
+  res.send('This crashed everything');
+  setTimeout(function () {
+    throw new Error('We crashed #2!');
+  }, 5);
+});
+
+app.get('/', (req, res) => {
+  console.log({ message: 'Hello from Express' });
+  res.send('Hello World for Stackdriver Error Reporting!');
+});
+
+app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
 );
